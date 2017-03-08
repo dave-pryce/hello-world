7b7c66f6473908d58e6498d9dfe9d94e5780e14e
Dave Pryce <dave.pryce.no1@gmail.com>
2017-03-08 14:43:32 +1000

    Updated son - removed comments

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ class App extends Component {
   // Initial state to hide skills
     this.state = {
       showSkills: false,
-      skills: [
-    //  { "id": 1,  "name": "Lean UX",  "description": "Agile with a Brain"},
-    //  {"id": 2, "name": "Design Sprint","description": "Google Stuff"},
-    //  {"id": 3, "name": "Second Gen Lean Prod Dev","description": "Economics"},
-    //  {"id": 4, "name": "Management 3.0","description": "Cool stuff from Jurgen Apello"}
-    ]
+      skills: []
     };
   }
 
@@ -46,7 +41,6 @@ class App extends Component {
       <div>
       <h3>Hello World</h3>
       <h4>Skills List</h4>
-      <SkillForm/>
       <button onClick={this._handleClick.bind(this)}>Toggle Skills</button>
       {skillNodes}
       </div>
@@ -92,30 +86,7 @@ class App extends Component {
     }
   }
 
-  class SkillForm extends React.component {
-  constructor() {
-  super();
-  render () {
-    return (
-    <form onSubmit={this._handleSubmit.bind(this)}>
-      <label>New Skill</label>
-      <div>
-       <input placerholder="Name:" ref={(input) => this._name = input}/>
-       <textarea placeholder="Description:" ref={(textarea) => this._description = textarea}></textarea>
-       <button>Add New Skill</button>
-     </div>
-     </form>
-    );
-  }
 
-  _handleSubmit(event) {
-    event.preventDefault();
-    let name = this._name;
-    let description = this._description
 
-    this.props.addSkill(name.value, description.value);
-  }
-  }
-};
 
 export default App;
